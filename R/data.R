42f75f89f18a90599aed2dd91f664ee95a73b08a
marcosziadi <139mziadi@gmail.com>
2024-10-16 21:49:13 -0300

    Documentacion de los conjuntos de datos

--- R/data.R
+++ R/data.R
@@ -4,8 +4,56 @@
 #'
 #' @format Los datos son obtenidos de un repositorio de GitHub.
 #' \describe{
+#' \item{NH0046}{muestra de datos de la estación "NH0098"}
+#' }
+#'
+#' @source Fuente: repositorio de GitHub.
+"NH0046"
+
+#' Segundo conjunto de datos
+#'
+#' Los datos contienen información de las distintas estaciones meteorológicas.
+#'
+#' @format Los datos son obtenidos de un repositorio de GitHub.
+#' \describe{
 #' \item{NH0098}{muestra de datos de la estación "NH0098"}
 #' }
 #'
 #' @source Fuente: repositorio de GitHub.
-"estaciones_merged"
+"NH0098"
+
+#' Segundo conjunto de datos
+#'
+#' Los datos contienen información de las distintas estaciones meteorológicas.
+#'
+#' @format Los datos son obtenidos de un repositorio de GitHub.
+#' \describe{
+#' \item{NH0437}{muestra de datos de la estación "NH0098"}
+#' }
+#'
+#' @source Fuente: repositorio de GitHub.
+"NH0437"
+
+#' Segundo conjunto de datos
+#'
+#' Los datos contienen información de las distintas estaciones meteorológicas.
+#'
+#' @format Los datos son obtenidos de un repositorio de GitHub.
+#' \describe{
+#' \item{NH0472}{muestra de datos de la estación "NH0098"}
+#' }
+#'
+#' @source Fuente: repositorio de GitHub.
+"NH0472"
+
+#' Segundo conjunto de datos
+#'
+#' Los datos contienen información de las distintas estaciones meteorológicas.
+#'
+#' @format Los datos son obtenidos de un repositorio de GitHub.
+#' \describe{
+#' \item{NH0910}{muestra de datos de la estación "NH0098"}
+#' }
+#'
+#' @source Fuente: repositorio de GitHub.
+"NH0910"
