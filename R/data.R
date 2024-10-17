cee33a37362934f50d06f67c257dab11b447c60e
marcosziadi <139mziadi@gmail.com>
2024-10-16 21:40:21 -0300

    Documentacion del paquete

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Segundo conjunto de datos
+#'
+#' Los datos contienen información de las distintas estaciones meteorológicas.
+#'
+#' @format Los datos son obtenidos de un repositorio de GitHub.
+#' \describe{
+#' \item{NH0098}{muestra de datos de la estación "NH0098"}
+#' }
+#'
+#' @source Fuente: repositorio de GitHub.
+"estaciones_merged"
